92acbede2a1cc952911b9b8992296783c75e0af6
matheus-sortech <matheus.almeida@sortech.com.br>
2025-05-09 00:10:10 -0300

    feat: EncrypterPort to UrlShortenerService contructor

--- src/main/java/com/br/shortener/url/domain/services/UrlShortenerService.java
+++ src/main/java/com/br/shortener/url/domain/services/UrlShortenerService.java
@@ -1,23 +1,36 @@
 package com.br.shortener.url.domain.services;
 
+import com.br.shortener.url.ports.outbound.EncrypterPort;
+import com.br.shortener.url.ports.outbound.UrlEncoderPort;
+
+// Encapsular depois esses metodos em um outro -> codificador_de_url()
+
 public class UrlShortenerService {
+    UrlEncoderPort urlEncoder;
+    EncrypterPort encrypter;
 
-    private String encodeToUtf8() {
-        //Convert the 128 bit hash value
+    public UrlShortenerService(UrlEncoderPort urlEncoderAdapter,
+                               EncrypterPort encrypter) {
+        this.urlEncoder = urlEncoderAdapter;
+        this.encrypter = encrypter;
+    }
 
-        return "";
+    private String encodeToUtf8(String url) {
+        return urlEncoder.encodeToUtf8(url);
     }
 
     private String appendSequencialNumber(String number) {
-        //Append sequencial number to the code
+        /* *
+        * Avaliar se vale a pena
+        * Seria necessário caso fosse decidido que urls iguais feitas por requisições diferentes
+        * necessitam serem diferentes (decidir)
+        * */
 
         return "";
     }
 
-    private String md5Hash(String number) {
-        //Encode with md5 hash
-
-        return "";
+    private String md5Hash(String toBeEncrypted) {
+        return encrypter.encrypt(toBeEncrypted);
     }
 
     private String base62Encode(String number) {
