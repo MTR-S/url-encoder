984e5c078034cbeabe94210eb300e79f4475e66a
matheus-sortech <matheus.almeida@sortech.com.br>
2025-05-09 00:07:35 -0300

    feat: Encrypter exception - NoSuchAlgorithmException. Added to the ExceptionHandler

--- src/main/java/com/br/shortener/url/domain/controllers/RestExceptionHandler.java
+++ src/main/java/com/br/shortener/url/domain/controllers/RestExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.br.shortener.url.domain.controllers;
 
 import com.br.shortener.url.exceptions.UnsupportedEncodingException;
+import com.br.shortener.url.exceptions.NoSuchAlgorithmException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.br.shortener.url.domain.dto.ApiErrorResponse;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -16,7 +17,13 @@ public class RestExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleUnsupportedEncodingException(UnsupportedEncodingException e) {
         return ResponseEntity.status(BAD_REQUEST).body(
-                new ApiErrorResponse(BAD_REQUEST.value(), "Encoding URL to UTF-8 " + e.getMessage()));
+                new ApiErrorResponse(BAD_REQUEST.value(), "Error encoding URL to UTF-8: " + e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleNoSuchAlgorithmException(NoSuchAlgorithmException e) {
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
+                new ApiErrorResponse(INTERNAL_SERVER_ERROR.value(), "Invalid or unsupported commit algorithm: " + e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
