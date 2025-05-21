07df630e8db4f7e82a03da209e36813f49595799
matheus-sortech <matheus.almeida@sortech.com.br>
2025-05-21 20:02:24 -0300

    fix: Exception classes specific

--- src/main/java/com/br/shortener/url/api/controllers/RestExceptionHandler.java
+++ src/main/java/com/br/shortener/url/api/controllers/RestExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.br.shortener.url.domain.controllers;
+package com.br.shortener.url.api.controllers;
 
 import com.br.shortener.url.exceptions.UnsupportedEncodingException;
 import com.br.shortener.url.exceptions.NoSuchAlgorithmException;
@@ -6,7 +6,7 @@ import com.br.shortener.url.exceptions.NoSuchAlgorithmException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import com.br.shortener.url.domain.dto.ApiErrorResponse;
+import com.br.shortener.url.api.dto.ApiErrorResponse;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -14,13 +14,13 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @ControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(UnsupportedEncodingException.class)
     public ResponseEntity<ApiErrorResponse> handleUnsupportedEncodingException(UnsupportedEncodingException e) {
         return ResponseEntity.status(BAD_REQUEST).body(
                 new ApiErrorResponse(BAD_REQUEST.value(), "Error encoding URL to UTF-8: " + e.getMessage()));
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(NoSuchAlgorithmException.class)
     public ResponseEntity<ApiErrorResponse> handleNoSuchAlgorithmException(NoSuchAlgorithmException e) {
         return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
                 new ApiErrorResponse(INTERNAL_SERVER_ERROR.value(), "Invalid or unsupported commit algorithm: " + e.getMessage()));
