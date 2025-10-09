6b069656f72d3a326b79baa25d7c2471cb79abc9
Matheus Almeida <matheusalmeidass14@gmail.com>
2025-10-09 19:23:33 -0300

    feat: new endpoint "/{shortCode}/info"

--- src/main/java/com/br/shortener/url/api/controllers/UrlShortenerController.java
+++ src/main/java/com/br/shortener/url/api/controllers/UrlShortenerController.java
@@ -1,12 +1,9 @@
 package com.br.shortener.url.api.controllers;
 
-import com.br.shortener.url.api.dto.ApiErrorResponse;
+import com.br.shortener.url.api.dto.*;
 
-import com.br.shortener.url.api.dto.ShortenUrlRequest;
 import com.br.shortener.url.domain.ports.outbound.StorageUrlPort;
 import com.br.shortener.url.domain.services.UrlShortenerService;
-import com.br.shortener.url.api.dto.ShortenUrlResponse;
-import com.br.shortener.url.domain.ports.outbound.StorageUrlPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +14,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.net.URI;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -41,7 +37,7 @@ public class UrlShortenerController {
     @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @PostMapping("/shorten")
-    public ResponseEntity<ShortenUrlResponse> postShortenUrl(@RequestBody ShortenUrlRequest request) {
+    public ResponseEntity<PostShortenUrlResponse> postShortenUrl(@RequestBody ShortenUrlRequest request) {
         String longUrl = request.getUrl();
         String shortCode = urlShortenerService.generateShortUrl(longUrl);
         // optionalExpirationTime Regex = Xd, Xm and Xs (X is a time quantifier, ex: 10d = 10 days)
@@ -49,7 +45,7 @@ public class UrlShortenerController {
 
         urlStorage.saveUrl(shortCode, longUrl, optionalExpirationTime);
 
-        ShortenUrlResponse response = new ShortenUrlResponse("URL criada com sucesso", shortCode);
+        PostShortenUrlResponse response = new PostShortenUrlResponse("URL criada com sucesso", shortCode);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -60,15 +56,27 @@ public class UrlShortenerController {
     @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @GetMapping("/{shortCode}")
-    public ResponseEntity<ShortenUrlResponse> redirect(@PathVariable String shortCode) {
+    public ResponseEntity<RedirectResponse> redirect(@PathVariable String shortCode) {
         String longUrl = urlStorage.getUrl(shortCode);
 
         if (longUrl == null || longUrl.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
-        ShortenUrlResponse response = new ShortenUrlResponse("URL encurtada identificada", longUrl);
+        RedirectResponse response = new RedirectResponse("URL encurtada identificada", longUrl);
 
         return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(longUrl)).body(response);
+    }
+
+    @Operation(summary = "Redirect to the original long url by passing a short code of the original url")
+    //@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @GetMapping("/{shortCode}/info")
+    public ResponseEntity<ShortCodeInfoResponse> shortCodeInfo(@PathVariable String shortCode) {
+        ShortCodeInfoResponse response = urlStorage.getShortUrlInfo(shortCode);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
