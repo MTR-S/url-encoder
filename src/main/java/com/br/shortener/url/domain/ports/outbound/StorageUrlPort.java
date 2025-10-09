e0413b09a6837d2a3aef74d1a34b1159383f4b6d
Matheus Almeida <matheusalmeidass14@gmail.com>
2025-10-09 19:24:08 -0300

    feat: new getShortUrlInfo() method

--- src/main/java/com/br/shortener/url/domain/ports/outbound/StorageUrlPort.java
+++ src/main/java/com/br/shortener/url/domain/ports/outbound/StorageUrlPort.java
@@ -1,6 +1,9 @@
 package com.br.shortener.url.domain.ports.outbound;
 
+import com.br.shortener.url.api.dto.ShortCodeInfoResponse;
+
 public interface StorageUrlPort {
     String getUrl(String shortCode);
     void saveUrl(String shortCode, String longUrl, Long expirationTime);
+    ShortCodeInfoResponse getShortUrlInfo(String shortCode);
 }
